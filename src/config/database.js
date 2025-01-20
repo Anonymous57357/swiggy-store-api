aecfae94b3590eca2f2633be18d6660cd6b2aef8
mukeshvijayakumar <mukesh@techasoft.com>
2025-01-20 16:57:54 +0530

    fix

--- src/config/database.js
+++ src/config/database.js
@@ -6,11 +6,15 @@ const connectDB = async () => {
       process.env.MONOGODB_CONNECTION_STRING,
       {
         dbName: process.env.DB_NAME,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
       }
     );
-    console.log("database connection successful");
+    console.log(`Database connected: ${connection.connection.host}`);
   } catch (error) {
-    console.error("Error conneciton to database ");
+    console.error("Error connecting to the database:");
+    console.error(error.message);
+    process.exit(1); // Exit the process if connection fails
   }
 };
 
